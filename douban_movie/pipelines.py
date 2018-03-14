c4025f8fe63f9662a19ccc77a3cd1cabbd57a5f8
shaoweicode <417096554@qq.com>
2018-03-14 14:18:39 +0800

    douban parser

--- douban_movie/pipelines.py
+++ douban_movie/pipelines.py
@@ -4,16 +4,19 @@ import re
 import redis
 import json
 import sys
+from scrapy.exceptions import DropItem
 
-class FlaskDocPipeline(object):
+class DoubanMoviePipeline(object):
     def process_item(self, item, spider):
         """
         TODO: 将 item 结果以 JSON 形式保存到 Redis 数据库的 list 结构中
         """        
-        data = json.dumps(item)
-        self.redis.lpush('douban_movie:items',data)
+        # data = json.dumps(item)
+        self.redis.lpush('douban_movie:items',item)
         if self.redis.llen('douban_movie:items')>35:
             print('已经有35条数据')
+        if float(item['score']<8.0):
+            raise DropItem('score is less than 8.0')
         return item
         
 
--- douban_movie/spiders/awesome_movie.py
+++ douban_movie/spiders/awesome_movie.py
@@ -4,7 +4,7 @@ import scrapy
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from douban_movie.items import MovieItem
-
+import re
 class AwesomeMovieSpider(scrapy.spiders.CrawlSpider):
 
     name = 'awesome-movie'
@@ -26,8 +26,9 @@ class AwesomeMovieSpider(scrapy.spiders.CrawlSpider):
         item = MovieItem()
         item['url'] = response.url
         item['name'] = response.css('title::text').extract()[0][0:-5].strip()
-        item['summary'] = response.css('div#link-report span::text').extract_first().rstrip()
-        item['score'] = response.css('strong.ll::text').extract()[0]
+        summary_text = response.css('span[property="v:summary"]::text').extract_first()
+        item['summary']=re.sub('\s','',summary_text)
+        item['score'] = response.css('strong[property="v:average"]::text').extract()[0]
         return item
 
     def parse_start_url(self, response):
@@ -36,3 +37,14 @@ class AwesomeMovieSpider(scrapy.spiders.CrawlSpider):
 
     def parse_page(self, response):
         yield self.parse_movie_item(response)
+
+
+
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
+
+# conda config --set show_channel_urls yes
