bf5b4076d26d95b8d2f550e30cb08f2149374544
shaoweicode <417096554@qq.com>
2018-03-14 15:26:52 +0800

    pipelines

--- douban_movie/pipelines.py
+++ douban_movie/pipelines.py
@@ -11,13 +11,21 @@ class DoubanMoviePipeline(object):
         """
         TODO: 将 item 结果以 JSON 形式保存到 Redis 数据库的 list 结构中
         """        
-        # data = json.dumps(item)
-        self.redis.lpush('douban_movie:items',item)
-        if self.redis.llen('douban_movie:items')>35:
-            print('已经有35条数据')
-        if float(item['score']<8.0):
+        
+
+        # if self.redis.llen('douban_movie:items')>35:
+        #     print('已经有35条数据')
+        if float(item['score'])<8.0:
             raise DropItem('score is less than 8.0')
-        return item
+        else:
+            data={
+                'url':item['url'],
+                'name':item['name'],
+                'summary':item['summary'],
+                'score':item['score']
+            }
+            self.redis.lpush('douban_movie:items',item)
+        # return item
         
 
     def open_spider(self, spider):
